702cdc45197126ad7dc52afcd0c4d5ecd873b8f4
Rakeshkirlampalli <rakeshkirlampalli432@gmail.com>
2024-01-06 14:59:41 +0530

    jihubh

---
+++ pickels-web/src/components/DeliveryData.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DeliveryData = () => {
+  return (
+    <Paper elevation={3} >
+        ,l;jkhkgjghf
+    </Paper>
+  )
+}
+
+export default DeliveryData
--- pickels-web/src/navComponents/Logout.jsx
+++ pickels-web/src/navComponents/Logout.jsx
@@ -1,15 +1,30 @@
 import React from 'react'
 import Paper from '@mui/material/Paper';
-const Logout = () => {
+import { useDispatch } from 'react-redux';
+import { clearShoppingData } from '../store/shoppingSlicer'
+const Logout = ({onLogout}) => {
+  const dispatch= useDispatch()
+    const handleLogout = () => {
+    localStorage.removeItem('Auth');
+    dispatch(clearShoppingData())
+    onLogout()
+  };
+  const handleMouseEnter = () => {
+    document.body.style.cursor = 'pointer';
+  };
+
+  const handleMouseLeave = () => {
+    document.body.style.cursor = 'default';
+  };
   return (
-    <Paper elevation={3} style={{ width: '160px', height: '120px', display: 'flex', flexDirection: 'column', borderRadius: '8px' ,color:'#363737'}}>
+    <Paper elevation={3}   onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} style={{ width: '160px', height: '120px', display: 'flex', flexDirection: 'column', borderRadius: '8px' ,color:'#363737' }}>
     <div style={{ width: '100%', height: '33.3%', display: 'flex', justifyContent: 'center', alignItems: 'center',  borderTopLeftRadius: '8px', borderTopRightRadius: '8px' }}>
       MyProfile
     </div>
     <div style={{ width: '100%', height: '33.3%', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
       MyProduct
     </div>
-    <div style={{ width: '100%', height: '33.3%', display: 'flex', justifyContent: 'center', alignItems: 'center', borderBottomLeftRadius: '8px', borderBottomRightRadius: '8px' }}>
+    <div onClick={handleLogout} style={{ width: '100%', height: '33.3%', display: 'flex', justifyContent: 'center', alignItems: 'center', borderBottomLeftRadius: '8px', borderBottomRightRadius: '8px' }}>
       Logout
     </div>
   </Paper>
