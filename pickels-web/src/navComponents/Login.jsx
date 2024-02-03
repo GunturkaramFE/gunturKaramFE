2d704943c0c5320f54189909ee21980898b029e3
subhashvarma <subhashvarma220@gmail.com>
2024-02-03 15:53:35 +0530

    added mobile nav

--- pickels-web/src/navComponents/Login.jsx
+++ pickels-web/src/navComponents/Login.jsx
@@ -1,22 +1,19 @@
 import React, { useState } from 'react';
-import { isValidEmail,isValidPassword } from '../helpers/validations';
-import bcrypt from 'bcryptjs';
-
-import PopupForm from '../Pop-up/PopupForm';
-import VerificationForm from '../Authorization/VerificationForm';
-import { HashPassword } from '../helpers/hashpassword';
+import { isValidEmail, isValidPassword } from '../helpers/validations';
 import api from '../api';
 import { fetchShoppingDataOnPageRefresh } from '../store/store';
+import VerificationForm from '../Authorization/VerificationForm';
 
-const Login = ({closeDrawer}) => {
-  const[ispop,setIsPopUp]=useState(false)
-
+const Login = ({ closeDrawer }) => {
+  const [ispop, setIsPopUp] = useState(false);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
-
-  const handleLogin = async() =>{
-   if (!isValidEmail(email)) {
+const closeHandler=()=>{
+  setIsPopUp(!ispop);
+}
+  const handleLogin = async () => {
+    if (!isValidEmail(email)) {
       setError('Invalid email format');
       return;
     }
@@ -24,21 +21,23 @@ const Login = ({closeDrawer}) => {
       setError('Enter Correct Password Format');
       return;
     }
-    const payload={
-    document:{
-      password:password,
-      email:email
-    }
-   }
-   const response = await api.post('/user/login', payload);
-try{
+
+    const payload = {
+      document: {
+        password: password,
+        email: email
+      }
+    };
+
+    try {
+      const response = await api.post('/user/login', payload);
+
       if (response.success === true) {
-       localStorage.setItem('Auth', response?.token)  
-       fetchShoppingDataOnPageRefresh() 
-       closeDrawer() 
-       
+        localStorage.setItem('Auth', response?.token);
+        fetchShoppingDataOnPageRefresh();
+        closeDrawer();
       } else if (response.success === false) {
-        alert(response.msg)
+        alert(response.msg);
       } else {
         setError('Something went wrong');
       }
@@ -46,19 +45,22 @@ try{
       setError('Confirmation failed due to a network error. Please try again.');
       console.log(error);
     }
+  };
 
-  }
-const [isHovered, setIsHovered] = useState(false);
-const handlePopup=()=>{
-setIsPopUp(!ispop)
-}
+  const [isHovered, setIsHovered] = useState(false);
+  const handlePopup = () => {
+  
+    setIsPopUp(!ispop);
+  };
+
   const linkStyle = {
     color: isHovered ? 'red' : 'green',
     textDecoration: 'none',
   };
+
   return (
-    <div style={{width:'100%',height:'100%'}}>
-      <div className="card-body p-md-1 mx-md-3">      
+    <div style={{ width: '100%', height: '100%' }}>
+      <div className="card-body p-md-1 mx-md-3">
         <form>
           <div className="form-outline mb-4">
             <label className="form-label" htmlFor="form2Example11">
@@ -87,32 +89,43 @@ setIsPopUp(!ispop)
             />
           </div>
 
-       {error && <p style={{ color: 'red' }}>{error}</p>}
-  <div className="text-center flex flex-col pt-1 mb-1 pb-1">
-  <button
-    className="btn btn-success btn-block fa-lg gradient-custom-2 mb-3"
-    type="button"
-    onClick={handleLogin}
-    style={{
-      backgroundColor: 'green', // Set the background color to green
-      color: 'white',            // Set the text color to white
-      // Add any other styles you want
-    }}
-  >
-    Log in
-  </button>
-  <a
-      href="#"
-      style={linkStyle}
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-  >
-      <u onClick={handlePopup}>Forgot Password?</u>
-    </a>
-    <PopupForm ispop={ispop} formData={<VerificationForm/>} fun={handlePopup} width='350'/>
-</div>      
-    </form>
-    </div>        
+          {error && <p style={{ color: 'red' }}>{error}</p>}
+          <div className="text-center flex flex-col pt-1 mb-1 pb-1">
+            <button
+              className="btn btn-success btn-block fa-lg gradient-custom-2 mb-3"
+              type="button"
+              onClick={handleLogin}
+              style={{
+                backgroundColor: 'green',
+                color: 'white',
+              }}
+            >
+              Log in
+            </button>
+            <a
+              href="#"
+              style={linkStyle}
+              onMouseEnter={() => setIsHovered(true)}
+              onMouseLeave={() => setIsHovered(false)}
+            >
+              <u onClick={handlePopup}>Forgot Password?</u>
+            </a>
+            {ispop && (
+              <div className="popup-overlay" style={{ zIndex: 10000000 }}>
+                <div className="popup-form" style={{ width: '350px', height: '400px' }}>
+                  <div style={{ display: 'flex', justifyContent: 'end' }}>
+                    <button className="close-button" onClick={handlePopup}>
+                      &times;
+                    </button>
+                  </div>
+                  <VerificationForm closeHandler={closeHandler} />
+
+                </div>
+              </div>
+            )}
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
