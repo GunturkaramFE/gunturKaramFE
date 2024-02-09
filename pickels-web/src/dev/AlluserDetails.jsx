efa4240053044dce533457948776e3b0053cc5af
Rakeshkirlampalli <rakeshkirlampalli432@gmail.com>
2024-02-09 16:08:54 +0530

    AdminChanged

--- pickels-web/src/dev/AlluserDetails.jsx
+++ pickels-web/src/dev/AlluserDetails.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
-import { Button, Card, CardContent, CircularProgress, FormControl, Grid, InputLabel, MenuItem, Select, TextField, Typography } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { Box, Button, Card, CardContent, CircularProgress, FormControl, Grid, InputLabel, MenuItem, Select, TextField, Typography } from '@mui/material';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import LocalShippingIcon from '@mui/icons-material/LocalShipping';
 import DetailsIcon from '@mui/icons-material/Details';
 import DeliveryDiningIcon from '@mui/icons-material/DeliveryDining';
 import api from '../api';
-
+import SentimentSatisfiedIcon from '@mui/icons-material/SentimentSatisfied';
 const defaultUser = {
   name: 'John Doe',
   email: 'john.doe@example.com',
@@ -18,36 +18,43 @@ const defaultUser = {
 };
 
 const AlluserDetails = ({ user = defaultUser }) => {
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [searchText, setSearchText] = useState('');
   const [searchType, setSearchType] = useState('id');
   const[shippingAddresses,setShippingAddress]=useState()
-  const[userdata,setUserdata]=useState()
+  const[userData,setUserData]=useState()
   const [orders,setOrders]=useState()
   
   const handleChange = (event) => {
     setSearchType(event.target.value);
   };
 
-  const handleSearch = async() => {
-
-try {
-  const payload={}
-  payload[searchType] = searchText;
-  const response= await api.post('/user/getUser',payload)
-  if(response.success){
-    setUserdata(response.user)
-    const address= await api.post('/user/getShippingAddress',{userId:response.user.id})
-    const orders= await api.post('/user/Order/get',{filter:{UserID:response.user.id}})
-    setOrders(orders.orders)
-    setShippingAddress(address.shippingAddresses)  
-  }  
-} catch (error) {
-  
-}finally{
-
-}
+  const handleSearch = async () => {
+    setLoading(true);
+    try {
+      const payload = {};
+      payload[searchType] = searchText;
+      const response = await api.post('/user/getUser', payload);
+      if (response.success) {
+        setUserData(response.user);
+        const address = await api.post('/user/getShippingAddress', { userId: response.user.id });
+        const ordersData = await api.post('/user/Order/get', { filter: { UserID: response.user.id } });
+        setOrders(ordersData.orders);
+        setShippingAddress(address.shippingAddresses);  
+      } else {
+        setUserData(null);
+        setOrders([]);
+        setShippingAddress([]);
+      }
+    } catch (error) {
+     
+    } finally {
+      setLoading(false);
+    }
   };
+  useEffect(()=>{
+    console.log(userData)
+  },[userData,orders,shippingAddresses])
 
   return (
     <>
@@ -98,11 +105,11 @@ try {
         </Grid>
       </Grid>
       
-      {loading?<div style={{ width: '100%', height: '80vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-          <CircularProgress />
-        </div>:<Grid container spacing={2}  md={12} sx={{display :'flex',justifyContent:'center',paddingTop:"20px ",fontFamily:'Tahoma'}}>
-        <Grid  item xs={12} sm={6} md={5.5} lg={5.5}>
-          <Card sx={{ height: {xs:"auto",md:"35vh",sm:"35vh",lg:'37vh'}, boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)' }}>
+      {loading?<Grid style={{ width: '100%', height: '70vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>      
+      <Typography variant="h4" sx={{ color: '#DAD0CE', }}>Find the User By Search results<SentimentSatisfiedIcon style={{width:"44px",height:'44px'}}/>!! </Typography>
+     </Grid>:<Grid container spacing={2}  md={12} sx={{display :'flex',justifyContent:'space-evenly',paddingTop:"20px ",fontFamily:'Tahoma'}}>
+        <Grid  item xs={12} sm={4} md={4} lg={4}>
+          <Card sx={{ height:'auto', boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)' }}>
             <CardContent>
               <Typography variant="h5" component="h2" align="center" justifyContent='center'>
                 My Profile <AccountCircleIcon style={{fontSize:'35px',color:'grey'}}/>
@@ -124,13 +131,14 @@ try {
             </CardContent>
           </Card>
         </Grid>
-        <Grid  item xs={12} sm={6} md={5.5} lg={5.5}>
-          <Card sx={{  height: {xs:"auto",md:"35vh",sm:"35vh",lg:'37vh'}, boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)' }}>
+        <Grid lg={6.5} sx={{display:"flex",flexDirection:'column',width:'100%',padding:'13px 0px',gap:"20px"}}> 
+        <Grid  item xs={12} sm={6} md={5.5} lg={12}>
+          <Card sx={{  height: {xs:"auto",md:"45vh",sm:"45vh",lg:'45vh'}, boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)' }}>
             <CardContent>
               <Typography variant="h5" component="h2" align="center" justifyContent='center'>
                 Order Details <DetailsIcon style={{fontSize:'30px'}}/>
               </Typography>
-              <Grid sx={{height:'100%',height:'28vh',overflowY:'auto'}} >
+              <Grid sx={{height:'100%',height:'35vh',overflowY:'auto'}} >
                 <Card sx={{padding:'20px',margin:'10px 0px',fontFamily:'Tahoma'}} >
                 <Typography sx={{fontSize:'18px'}}  >OrderID:nbo15jk2b1</Typography>
                     <Typography>Status : Placed</Typography>
@@ -154,13 +162,13 @@ try {
             </CardContent>
           </Card>
         </Grid>
-        <Grid  item xs={12} sm={6} md={5.5} lg={5.5}>
-          <Card sx={{  height: {xs:"auto",md:"35vh",sm:"35vh",lg:'37vh'}, boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)' }}>
+        <Grid  item xs={12} sm={6} md={5.5} lg={12}>
+          <Card sx={{  height: {xs:"auto",md:"45vh",sm:"45vh",lg:'45vh'}, boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)' }}>
             <CardContent>
               <Typography variant="h5" component="h2" align="center">
                Delivery Address <LocalShippingIcon style={{fontSize:'30px'}}/>
               </Typography>
-              <Grid sx={{height:'100%',height:'28vh',overflowY:'auto'}}>
+              <Grid sx={{height:'100%',height:'35vh',overflowY:'auto'}}>
                 <Card sx={{padding:'20px',margin:'10px 0px',fontFamily:'Tahoma'}}>
                     <Typography variant='h6' >Rakesh Kirlampalli</Typography>
                     <Typography >Mobile: 9849765863 </Typography>
@@ -182,33 +190,8 @@ try {
             </CardContent>
           </Card>
         </Grid>
-        <Grid  item xs={12} sm={6} md={5.5} lg={5.5}>
-          <Card sx={{  height: {xs:"auto",md:"35vh",sm:"35vh",lg:'37vh'}, boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)' }}>
-            <CardContent>
-              <Typography variant="h5" component="h2" align="center">
-                Shipping Address <DeliveryDiningIcon  style={{fontSize:'30px'}}/>
-              </Typography>
-              <Grid sx={{height:'100%',height:'28vh',overflowY:'auto'}}>
-                <Card sx={{padding:'20px',margin:'10px 0px'}}>
-                    <Typography variant='h6'>Rakesh Kirlampalli</Typography>
-                    <Typography>Mobile: 9849765863 </Typography>
-                    <Typography>85-155, Ramalayam, visakhapatnam, Andhra Pradesh, India ,pincode: 531011</Typography>
-                </Card>
-                <Card sx={{padding:'20px',margin:'10px 0px'}}>
-                    <Typography variant='h6'>Rakesh Kirlampalli</Typography>
-                    <Typography>Mobile: 9849765863 </Typography>
-                    <Typography>85-155, Ramalayam, visakhapatnam, Andhra Pradesh, India ,pincode: 531011</Typography>
-                </Card>
-                <Card sx={{padding:'20px',margin:'10px 0px'}}>
-                    <Typography variant='h6'>Rakesh Kirlampalli</Typography>
-                    <Typography>Mobile: 9849765863 </Typography>
-                    <Typography>85-155, Ramalayam, visakhapatnam, Andhra Pradesh, India ,pincode: 531011</Typography>
-                </Card>
-          
-              </Grid>
-            </CardContent>
-          </Card>
         </Grid>
+
       </Grid>}
   
     </>
